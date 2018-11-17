98849ee0f3838e805c331ac45cc71e5db8f2f41c
sebastian <sebastian.e.blanco@gmail.com>
2018-11-17 18:08:51 -0300

    fix launch file

--- launch.py
+++ launch.py
@@ -1,23 +1,18 @@
 def launch ():
     import pox.log.color
+    pox.log.color.launch()
     import pox.log
-    import pox.log.level
-    import logging
+    pox.log.launch(format="[@@@bold@@@level%(name)-22s@@@reset] " +
+                          "@@@bold%(message)s@@@normal")
     from pox.core import core
     import pox.openflow.discovery
-    import pox.openflow.spanning_tree
-    import pox.host_tracker
-    import controller
-
-    pox.log.color.launch()
-    pox.log.launch(
-        format=
-        "[@@@bold@@@level%(name)-22s@@@reset] " + "@@@bold%(message)s@@@normal")
     pox.openflow.discovery.launch()
-    pox.log.level.launch(packet=logging.WARN, host_tracker=logging.INFO)
+
+    import controller as c
     core.getLogger("openflow.spanning_tree").setLevel("INFO")
-    name = controller.MyController.__name__
-    core.getLogger().debug("Using forwarding: %s", name)
-    controller.launch()
+    core.getLogger().debug("Using forwarding: %s", c.__name__)
+    c.launch()
+
+    import pox.openflow.spanning_tree
     pox.openflow.spanning_tree.launch()
-    pox.host_tracker.launch()
+
