8b48f68ef467d86ec94015d18df52b568eecc3c1
sebastian <sebastian.e.blanco@gmail.com>
2018-11-19 06:04:35 -0300

    add new files to copy

--- resync.sh
+++ resync.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 sshpass -p "frenetic" rsync -av -e ssh --exclude='*.ova' /home/sebastian/git/Datacenter/ frenetic@192.168.56.3:~/Datacenter/
-sshpass -p "frenetic" rsync -av -e ssh /home/sebastian/git/Datacenter/controller.py launch.py frenetic@192.168.56.3:~/pox/ext/
+sshpass -p "frenetic" rsync -av -e ssh /home/sebastian/git/Datacenter/controller.py launch.py data_definitions.py entry.py frenetic@192.168.56.3:~/pox/ext/
+
 
