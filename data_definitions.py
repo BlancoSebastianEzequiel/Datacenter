2168320f4433899f2bcc49abe8106ac9b90b15a2
sebastian <sebastian.e.blanco@gmail.com>
2018-11-19 06:02:46 -0300

    add constants file

---
+++ data_definitions.py
@@ -0,0 +1,11 @@
+# Timeout for flows
+FLOW_IDLE_TIMEOUT = 10
+
+# Timeout for ARP entries
+ARP_TIMEOUT = 60 * 2
+
+# Maximum number of packet to buffer on a switch for an unknown IP
+MAX_BUFFERED_PER_IP = 5
+
+# Maximum time to hang on to a buffer for an unknown IP in seconds
+MAX_BUFFER_TIME = 5
