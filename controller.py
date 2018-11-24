09bea301a2eb25c3bda15b1a274f6b555ef8ab57
sebastian <sebastian.e.blanco@gmail.com>
2018-11-24 06:16:39 -0300

    use packet dst to get entry and use host_tracker

--- controller.py
+++ controller.py
@@ -1,5 +1,4 @@
 from pox.core import core
-from pox.host_tracker.host_tracker import host_tracker
 import pox.openflow.libopenflow_01 as of
 from pox.lib.util import dpidToStr
 import pox.lib.packet as pkt
@@ -7,41 +6,46 @@ from pox.lib.revent import *
 
 log = core.getLogger()
 
+
 class Controller(object):
 
     def __init__(self):
         core.openflow.addListeners(self)
+        self.event = None
+        self.dpid = None
+        self.in_port = None
+        self.packet = None
         self.dst_dpid = None
         self.out_port = None
         self.table = {}
-        self.host_tracker = host_tracker()
         self.eth_packet = None
         self.ip_packet = None
         self.arp_packet = None
         self.icmp_packet = None
         self.tcp_packet = None
         self.udp_packet = None
+        self.net_packet = None
+        self.protocol_packet = None
+        self.protocol = None
+        self.arp_table = {}
+        self.is_ip = True
 
     def _handle_ConnectionUp(self, event):
         log.debug("Connection %s" % (event.connection,))
+        self.print_msg("EVENT: %s" % event.connection.__class__.__name__)
 
-        # multicast
         msg = of.ofp_flow_mod()
         msg.match.dl_dst = pkt.ETHER_BROADCAST
         msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
         event.connection.send(msg)
 
-        # arp
+        msg = of.ofp_flow_mod()
+        msg.match.dl_type = pkt.ethernet.IPV6_TYPE
+        event.connection.send(msg)
+
         msg = of.ofp_flow_mod()
         msg.match.dl_type = pkt.ethernet.ARP_TYPE
         msg.actions.append(of.ofp_action_output(port=of.OFPP_FLOOD))
-        event.connection.send(msg)
-
-        self.drop_ipv6(event)
-
-    def drop_ipv6(self, event):
-        msg = of.ofp_flow_mod()
-        msg.match.dl_type = pkt.ethernet.IPV6_TYPE
         event.connection.send(msg)
 
     @staticmethod
@@ -59,19 +63,32 @@ class Controller(object):
             log.warning(
                 "%i %i ignoring unparsed packet" % (self.dpid, self.in_port))
             return
-        if not self.validate_packets():
+        self.eth_packet = self.packet.find(pkt.ethernet)
+        self.ip_packet = self.packet.find(pkt.ipv4)
+        self.arp_packet = self.packet.find(pkt.arp)
+        self.icmp_packet = self.packet.find(pkt.icmp)
+        self.tcp_packet = self.packet.find(pkt.tcp)
+        self.udp_packet = self.packet.find(pkt.udp)
+
+        if not self.validate_protocols():
             return
-        self.flood()
-        entry = self.host_tracker.getMacEntry(self.eth_packet.dst)
+        if not self.validate_net_packets():
+            return
+
+        entry = core.host_tracker.getMacEntry(self.packet.dst)
+
         if entry is None:
             log.warning("Could not find dst")
             return self.flood()
 
         self.dst_dpid = entry.dpid
-        if self.dpid == self.dpid:
+        if self.dpid == self.dst_dpid:
             log.info("Current switch is destination")
             self.out_port = entry.port
         else:
+            if self.packet.dst.is_multicast:
+                self.print_msg("MULTICAST")
+                return self.flood()
             log.info("Finding minimun paths")
             minimun_paths = self.get_minimun_paths()
             log.info("finding out port")
@@ -79,10 +96,10 @@ class Controller(object):
             if self.out_port is None:
                 log.info("could not find out port")
                 return
-            log.info("Updating flow table")
-            self.update_table()
-            log.info("Sending packet")
-            self.send_packet()
+        log.info("Updating flow table")
+        self.update_table()
+        log.info("Sending packet")
+        self.send_packet()
 
     def flood(self):
         dpid = dpidToStr(self.dpid)
@@ -94,46 +111,95 @@ class Controller(object):
         msg.in_port = self.in_port
         self.event.connection.send(msg)
 
-    def validate_packets(self):
-        self.eth_packet = self.packet.find(pkt.ethernet)
-        self.ip_packet = self.packet.find(pkt.ipv4)
-        self.arp_packet = self.packet.find(pkt.arp)
-        self.icmp_packet = self.packet.find(pkt.icmp)
-        self.tcp_packet = self.packet.find(pkt.tcp)
-        self.udp_packet = self.packet.find(pkt.udp)
-        if [self.icmp_packet, self.tcp_packet, self.udp_packet] == [None]*3:
+    def validate_protocols(self):
+        if self.udp_packet is not None:
+            log.info("UDP packet!")
+            self.protocol = "UDP"
+            self.protocol_packet = self.udp_packet
+            return True
+        elif self.tcp_packet is not None:
+            log.info("TCP packet!")
+            self.protocol = "TCP"
+            self.protocol_packet = self.tcp_packet
+            return True
+        elif self.icmp_packet is not None:
+            log.info("ICMP packet!")
+            self.protocol = "ICMP"
+            self.protocol_packet = self.icmp_packet
+            return True
+        else:
             log.warning("icmp, tcp and udp packets are None!")
+            return False
+
+    def validate_net_packets(self):
+        if self.eth_packet is None:
+            log.warning("ETHERNET packet is None!")
+            return False
+        if self.ip_packet is not None:
+            log.info("IP packet!")
+            self.is_ip = True
+            self.net_packet = self.ip_packet
+        elif self.arp_packet is not None:
+            log.info("ARP packet!")
+            self.is_ip = False
+            self.net_packet = self.arp_packet
+        else:
+            log.warning("ARP and TCP packets are None!")
             return False
         return True
 
-    def save_packet(self, packet, msg):
-        if self.tcp_packet is None:
-            return
-        msg.match.tp_src = packet.srcport
-        msg.match.tp_dst = packet.dstport
+    def match_protocol_packets(self, msg):
+        if self.is_ip:
+            msg.match.nw_src = self.net_packet.srcip
+            msg.match.nw_dst = self.net_packet.dstip
+            msg.match.nw_proto = self.net_packet.protocol
+            return msg
+        msg.match.nw_src = self.net_packet.protosrc
+        msg.match.nw_dst = self.net_packet.protodst
+        msg.match.nw_proto = self.net_packet.prototype
+        return msg
+
+    def match_packet(self, msg):
+        if not self.is_ip:
+            return msg
+        msg.match.nw_src = self.ip_packet.srcip
+        msg.match.nw_dst = self.ip_packet.dstip
+        msg.match.nw_proto = self.ip_packet.protocol
+        return msg
 
     def update_table(self):
         msg = of.ofp_flow_mod()
         msg.match.dl_type = self.eth_packet.type
-        msg.match.nw_src = self.ip_packet.srcip
-        msg.match.nw_dst = self.ip_packet.dstip
-        msg.match.nw_proto = self.ip_packet.protocol
-        self.save_packet(self.tcp_packet, msg)
-        self.save_packet(self.udp_packet, msg)
-        msg.actions.append(of.ofp_action_output(port=self.in_port))
+        msg = self.match_packet(msg)
+        if self.protocol != "ICMP":
+            msg.match.tp_src = self.protocol_packet.srcport
+            msg.match.tp_dst = self.protocol_packet.dstport
+        msg.actions.append(of.ofp_action_output(port=self.out_port))
         self.event.connection.send(msg)
         self.balance_of_charges()
 
     def get_minimun_paths(self):
-        paths = [[neighbour] for neighbour in self.get_adjacents(self.dpid)]
+        begin = {"dpid": self.dpid, "port": self.in_port}
+        adjacents = self.get_adjacents(self.dpid)
+        paths = [[begin, neighbour] for neighbour in adjacents]
         paths_to_dst = []
         while not paths_to_dst:
-            for path in paths:
+            last_paths = paths[:]
+            for path in last_paths:
                 adjacents = self.get_adjacents(path[-1]["dpid"])
                 for an_adjacent in adjacents:
-                    paths.append(path + [an_adjacent])
+                    if not self.node_belongs_path(an_adjacent, path):
+                        paths.append(path + [an_adjacent])
             paths_to_dst = self.filter_paths(paths, self.dst_dpid)
         return paths_to_dst
+
+    def node_belongs_path(self, node, path):
+        dpid = node["dpid"]
+        port = node["port"]
+        for a_node in path:
+            if a_node["dpid"] == dpid and a_node["port"] == port:
+                return True
+        return False
 
     def get_out_port(self, paths_to_dst):
         if len(paths_to_dst) == 0:
@@ -141,13 +207,20 @@ class Controller(object):
         return self.get_port_applying_ecmp(self.get_all_ports(paths_to_dst))
 
     def get_port_applying_ecmp(self, ports):
+        key = (self.dpid, self.dst_dpid, self.protocol)
+        if key not in self.table:
+            self.table[key] = ports[0]
+            return ports[0]  # random
         for port in ports:
-            if self.table[(self.dpid, self.dst_dpid)][self.tcp_packet] == port:
+            if self.table[key] == port:
                 return port
         return ports[0]  # random
 
     def balance_of_charges(self):
-        self.table[(self.dpid, self.dst_dpid)][self.tcp_packet] = self.out_port
+        log.info("saving (%s, %s, %s): %s" %
+                 (self.dpid, self.dst_dpid, self.protocol, self.out_port))
+        key = (self.dpid, self.dst_dpid, self.protocol)
+        self.table[key] = self.out_port
 
     @staticmethod
     def get_all_ports(paths_to_dst):
@@ -166,12 +239,16 @@ class Controller(object):
     def get_adjacents(dpid):
         adjacents = []
         for an_adjacent in core.openflow_discovery.adjacency:
-            if an_adjacent.dpi1 != dpid:
-                continue
-            adjacents.append({
-                "dpid": an_adjacent.dpid2,
-                "port": an_adjacent.port2
-            })
+            if an_adjacent.dpid1 == dpid:
+                adjacents.append({
+                    "dpid": an_adjacent.dpid2,
+                    "port": an_adjacent.port2
+                })
+            elif an_adjacent.dpid2 == dpid:
+                adjacents.append({
+                    "dpid": an_adjacent.dpid1,
+                    "port": an_adjacent.port1
+                })
         return adjacents
 
     def send_packet(self):
@@ -180,7 +257,3 @@ class Controller(object):
         msg.data = self.event.ofp
         msg.in_port = self.in_port
         self.event.connection.send(msg)
-
-
-def launch():
-    core.registerNew(Controller)
