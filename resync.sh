7ef64702e512c94182fe3b34bb074ba4278bbd90
sebastian <sebastian.e.blanco@gmail.com>
2018-12-01 03:00:26 -0300

    add ecmp table to be copied to VM

--- resync.sh
+++ resync.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sshpass -p "frenetic" rsync -av -e ssh --exclude='*.ova' /home/sebastian/git/Datacenter/ frenetic@192.168.56.3:~/Datacenter/
-sshpass -p "frenetic" rsync -av -e ssh /home/sebastian/git/Datacenter/controller.py firewall.py launch.py frenetic@192.168.56.3:~/pox/ext/
+sshpass -p "frenetic" rsync -av -e ssh /home/sebastian/git/Datacenter/controller.py firewall.py ecmp_table.py launch.py frenetic@192.168.56.3:~/pox/ext/
 
 
