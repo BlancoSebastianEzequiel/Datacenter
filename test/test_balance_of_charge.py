76e2f97381f45a6ce8480068131a27107057836b
sebastian <sebastian.e.blanco@gmail.com>
2018-12-01 02:58:16 -0300

    add test that check wireshark output

---
+++ test/test_balance_of_charge.py
@@ -0,0 +1,26 @@
+import os
+import utils
+
+
+def test_tcp_balance_of_charges():
+    os.system(
+        "tshark -T fields -E header=y -e ip.src -e ip.dst -e _ws.col.Protocol "
+        "-r wireshark/s2-eth1-iperf-h1-h4.pcap  > test/s2-h1-h4.txt")
+    os.system(
+        "tshark -T fields -E header=y -e ip.src -e ip.dst -e _ws.col.Protocol "
+        "-r wireshark/s3-eth1-iperf-h1-h4.pcap  > test/s3-h1-h4.txt")
+    df_1 = utils.get_df("test/s2-h1-h4.txt")
+    df_1.columns = ['src', 'dst', 'protocol']
+
+    def f(x):
+        return x['dst'] == '10.0.0.4' and x['protocol'] == 'TCP'
+
+    df_1 = utils.filter_by(df_1, ['dst', 'protocol'], f)
+    size_1 = len(df_1.index)
+
+    df_2 = utils.get_df("test/s3-h1-h4.txt")
+    df_2.columns = ['src', 'dst', 'protocol']
+    df_2 = utils.filter_by(df_2, ['dst', 'protocol'], f)
+    size_2 = len(df_2.index)
+
+    assert (size_1 == 0 and size_2 != 0) or (size_1 != 0 and size_2 == 0)
---
+++ test/utils.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+np.warnings.filterwarnings('ignore')
+
+
+def get_df(file_path):
+    return pd.read_csv(file_path, low_memory=False, sep='\t')
+
+
+def filter_ip(df, ip, ip_src_col, ip_dst_col):
+    return df[(df[ip_src_col] == ip) | (df[ip_dst_col] == ip)]
+
+
+def filter_by(df, values, callback):
+    return df[df[values].apply(lambda x: callback(x), axis=1)]
+
+
+def rtt_field_to_float(x, rtt_col):
+    if x[rtt_col] == 'None':
+        x[rtt_col] = None
+        return x
+    x[rtt_col] = float(x[rtt_col])
+    return x
